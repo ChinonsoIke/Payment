0a5f0844b46313d5eb317a00c066c53dd824fd20
ChinonsoIke <chinonsoikewelugo@gmail.com>
2022-09-21 06:33:13 +0100

    rerun migrations

---
+++ Payment.Infrastructure/Migrations/20220921021340_AddPinAndCustomerCodeToWallet.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Payment.Infrastructure.Migrations
+{
+    public partial class AddPinAndCustomerCodeToWallet : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CustomerCode",
+                table: "Wallets",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Pin",
+                table: "Wallets",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CustomerCode",
+                table: "Wallets");
+
+            migrationBuilder.DropColumn(
+                name: "Pin",
+                table: "Wallets");
+        }
+    }
+}
